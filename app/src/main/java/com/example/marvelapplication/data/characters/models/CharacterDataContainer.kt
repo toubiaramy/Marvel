d6245bc58ee7e285e001f8875b659252e62dc0d2
toubiaramy <113016647+toubiaramy@users.noreply.github.com>
2022-10-20 18:49:03 +0300

    adding room database dependency to store characters locally

--- app/src/main/java/com/example/marvelapplication/data/characters/models/CharacterDataContainer.kt
+++ app/src/main/java/com/example/marvelapplication/data/characters/models/CharacterDataContainer.kt
@@ -3,9 +3,9 @@ package com.example.marvelapplication.data.characters.models
 import java.io.Serializable
 
 class CharacterDataContainer(
-    //val offset: Int,
-    //val limit: Int,
-    //val total: Int,
-    //val count: Int,
+    // val offset: Int,
+    // val limit: Int,
+    // val total: Int,
+    // val count: Int,
     val results: ArrayList<Character>
 ) : Serializable
--- app/src/main/java/com/example/marvelapplication/data/characters/models/CharacterDataWrapper.kt
+++ app/src/main/java/com/example/marvelapplication/data/characters/models/CharacterDataWrapper.kt
@@ -3,11 +3,11 @@ package com.example.marvelapplication.data.characters.models
 import java.io.Serializable
 
 class CharacterDataWrapper(
-    //val code: Int,
-    //val status: String,
-    //val copyright: String,
-    //val attributionText: String,
-    //val attributionHTML: String,
+    // val code: Int,
+    // val status: String,
+    // val copyright: String,
+    // val attributionText: String,
+    // val attributionHTML: String,
     val data: CharacterDataContainer
-    //val etag: String
+    // val etag: String
 ) : Serializable
